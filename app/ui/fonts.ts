a071b39be48f171c57af86fc2ccdb526b988a520
rachel <rachel.uiux@gmail.com>
2023-11-14 17:11:51 +0900

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
